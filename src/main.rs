020bd7f0803288b84f11fd9bec8bd170c83d5f98
vicat <vicat47@qq.com>
2023-06-27 02:09:01 +0800

    ✨: 添加数据库连接

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use actix_web::{middleware, App, HttpServer};
+use std::env;
+
+use actix_web::{middleware, App, HttpServer, web};
 use chatgpt_agent::app_config::config_app;
+use sqlx::{postgres::PgPoolOptions, types::chrono::{DateTime, Local}};
+use dotenv::dotenv;
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct SysUserToken {
+    pub id: i32,
+    pub name: String,
+    pub local_token: String,
+    pub gpt_token: String,
+    pub create_time: DateTime<Local>, 
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     log::info!("starting HTTP server at http://localhost:8080");
 
-    HttpServer::new(|| {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&env::var("DATABASE_URL").unwrap()).await.unwrap();
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    let row = sqlx::query_as::<_, SysUserToken>(r#"SELECT id, name, local_token, gpt_token, create_time from "sys_user_token" where local_token=$1 "#)
+        .bind("123123")
+        .fetch_one(&pool).await.unwrap();
+
+    let shared_data = web::Data::new(pool);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(shared_data.clone())
             .configure(config_app)
             // enable logger
             .wrap(middleware::Logger::default())
