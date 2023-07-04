969c822760d62dfa7e7b76fbaa36b241924160c7
张尚斌 <zhangshangbin01@spic.com.cn>
2023-07-04 13:26:36 +0800

    🔧: 修复获取不到 token 的 bug

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -13,7 +13,7 @@ pub fn get_auth_token(req: HttpRequest) -> Result<String, Error> {
         .and_then(|d| Some(d.to_str()))
         .ok_or_else(|| actix_web::error::ErrorUnauthorized("dont have token"))?
         .map_err(|_| actix_web::error::ErrorUnauthorized("token error"))?;
-    if auth.starts_with("Bearer") || auth.len() < "Bearer".len() + 5 {
+    if !auth.starts_with("Bearer") || auth.len() < "Bearer".len() + 5 {
         return Err(actix_web::error::ErrorUnauthorized("not a valid bearer token"));
     }
     Ok(auth["Bearer".len()..].trim().to_string())
